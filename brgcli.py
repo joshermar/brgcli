3e0c887d3ee054eaf22d8b1a31cd48d014b83260
joshermar <joshermar@gmail.com>
2019-04-17 10:46:27 -0500

    Added ctrl-c functionality and placed curses statements inside try statement, where they belong

--- brgcli.py
+++ brgcli.py
@@ -2,19 +2,6 @@
 
 import curses
 
-# get the curses screen window
-screen = curses.initscr()
-
-# turn off input echoing and disable cursor
-curses.noecho()
-curses.curs_set(0)
-
-# respond to keys immediately (don't wait for enter)
-curses.cbreak()
-
-# map arrow keys to special values
-screen.keypad(True)
-
 # Set max brightness value
 max_brightness = 7500
 
@@ -22,11 +9,25 @@ max_brightness = 7500
 with open('/sys/class/backlight/intel_backlight/brightness', 'r') as file:
     brightness = file.read()
 
-screen.addstr(0, 0, 'Current brightness: ' + str(brightness), curses.A_REVERSE)
-
 brightness = round(int(brightness) / 25) * 25
 
 try:
+    # get the curses screen window
+    screen = curses.initscr()
+
+    # turn off input echoing, disable cursor, and respond to keys
+    # immediately (don't wait for enter)
+    curses.noecho()
+    curses.curs_set(0)
+    curses.cbreak()
+
+    # map arrow keys to special values
+    screen.keypad(True)
+
+    # Initial screen... :/
+    screen.addstr(
+        0, 0, 'Current brightness: ' + str(brightness), curses.A_REVERSE)
+
     while True:
         char = screen.getch()
         if char == ord('q'):
@@ -57,10 +58,13 @@ try:
         screen.erase()
         screen.addstr(
             0, 0, 'Current brightness: ' + str(brightness), curses.A_REVERSE)
+
+except KeyboardInterrupt:
+    pass
+
 finally:
     # shut down cleanly
     curses.nocbreak()
-    screen.keypad(0)
     curses.echo()
     curses.curs_set(1)
     curses.endwin()
